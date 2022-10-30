3018c98fdacd9f766ae2812062f3b03a3999867b
Alexey Levchuk <bizlex@yandex.ru>
2022-10-30 16:26:40 +0300

    Update lwc specialist project

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,20 @@
+declare module "@salesforce/label/c.Add_Review" {
+    var Add_Review: string;
+    export default Add_Review;
+}
+declare module "@salesforce/label/c.Details" {
+    var Details: string;
+    export default Details;
+}
+declare module "@salesforce/label/c.Full_Details" {
+    var Full_Details: string;
+    export default Full_Details;
+}
+declare module "@salesforce/label/c.Please_select_a_boat" {
+    var Please_select_a_boat: string;
+    export default Please_select_a_boat;
+}
+declare module "@salesforce/label/c.Reviews" {
+    var Reviews: string;
+    export default Reviews;
+}
